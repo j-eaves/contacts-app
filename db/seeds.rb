3b1911a04b018adb22e5760ab33892b5b2ba1c12
Brian <riceb53@gmail.com>
2017-03-06 22:54:37 -0500

    add view to see all contacts. add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,11 @@
+# make 100 fake people
+contact1 = 0
+100.times do
+  p 'in the loop'
+  contact1 = Contact.new({first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: Faker::PhoneNumber.phone_number, email: Faker::Internet.email})
+end
+contact1.save
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
